80979f20404e4a1410a1f5aae13bf16d0b386011
Dashiell Stander <dstander@protonmail.com>
2022-02-10 20:09:55 -0800

    hardcoded some things, sue me

--- ttig/main.py
+++ ttig/main.py
@@ -1,7 +1,8 @@
-from os.path import abspath, dirname
+from os.path import abspath, dirname, join
 import sys
 
-sys.path.append(abspath(dirname(dirname(__file__)))) # TODO: Proper packaging so this isn't necessary
+proj_dir = abspath(dirname(dirname(__file__)))
+sys.path.append(proj_dir) # TODO: Proper packaging so this isn't necessary
 
 from ttig.mmfid import make_reference_statistics, calc_mmfid_from_model
 from ttig.models.model import MultiModalFeatureExtractor
@@ -13,13 +14,25 @@ app = typer.Typer()
 
 
 @app.command()
-def mmfid(checkpoint_path: str, config_path: str, data_fp: str, num_samples: int = 524_288, batch_size: int = 128):
+def mmfid(data_fp: str, ref_stats_name='coco3m_total', num_samples: int = 524_288, batch_size: int = 128):
+    model_name = 'vqgan_imagenet_f16_16384'
+    checkpoint_path = join(proj_dir, 'checkpoints', 'vqgan', f'{model_name}.ckpt')
+    config_path = join(proj_dir, 'checkpoints', 'vqgan', f'{model_name}.yaml')
     config = VQGANConfig()
     stats_model = MultiModalFeatureExtractor()
     stats_model.to('cuda')
     vqgan = VqGanClipGenerator(checkpoint_path, config_path, config)
     vqgan.to('cuda')
-    calc_mmfid_from_model(data_fp, vqgan, stats_model, '', '', batch_size, num_samples, save_images = True)
+    calc_mmfid_from_model(
+        data_fp,
+        vqgan,
+        stats_model,
+        'vqgan_imagenet',
+        ref_stats_name,
+        batch_size,
+        num_samples,
+        save_images = True
+    )
 
 
 @app.command()
